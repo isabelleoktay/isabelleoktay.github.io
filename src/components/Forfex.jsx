e9f4f86da89816a693597ff9a6cc0dc38473e33e
isabelleoktay <60681704+isabelleoktay@users.noreply.github.com>
2022-04-23 21:54:06 -0400

    made first poem

---
+++ src/components/Forfex.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Forfex = (props) => {
+    return (
+        <div className='flex h-screen justify-center items-center'>
+            <div className='text-center tracking-wide font-mono text-[#32CD32]'>
+                <p>hope for</p>
+                <p>Earth becalmed</p>
+                <p className='mb-20'>time embodied</p>
+                <p className='hover:font-extrabold hover:drop-shadow-xl hover:text-lg hover:cursor-pointer'>possibility</p>
+                <p className='mt-20'>stillness twisted</p>
+                <p>knots into</p>
+                <p>the yarn</p>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Forfex;
